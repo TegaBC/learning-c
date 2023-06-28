63a5aec0b889b44064af667659a388116e535921
TegaBC <jjbencarew10@gmail.com>
2023-06-28 09:03:09 +0100

    hello world program.

---
+++ getting_started/hello.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+// Prints the message to console, %s is replaced with the variable "name" (format specifier)
+int main() {
+    char name[] = "Tega";
+
+    printf("Hello world! Your name is %s.", name);
+
+    return 0;
+}
