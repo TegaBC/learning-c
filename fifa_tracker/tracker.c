fb56d5c8b0b4a6599a19be6522365ea4735c6c47
TegaBC <64938182+TegaBC@users.noreply.github.com>
2023-07-06 04:25:41 +0100

    building out functions

---
+++ fifa_tracker/tracker.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <string.h>
+
+typedef struct 
+{
+    char name[30];
+    int overall;
+} Player;
+
+// Players that are currently saved
+Player currentPlayers[];
+
+void viewPlayers() {
+    // Print to console the players, with colours n border
+}
+
+void editPlayer() {
+    // Give options on editing players
+}
+
+void addNewPlayer(Player player) {
+    // Go to new player form
+}
+
+int main() {
+
+    // Ask user to enter a command (view, edit, add)
+
+    return 0;
+}
